e6b41dc529ca52a15b3ecaed5d8feb359f6a24f5
SugarShootingStar <ryu.8224@gmail.com>
2018-11-11 20:49:19 +0900

    feat:puppeteerのサンプルコードfix

---
+++ src/Node/puppeteer/index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+const URL = 'https://gigazine.net/';
+
+async function main(){
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(URL);
+
+  const title = await page.title();
+  const newsListSelector = '#section > div > section > div > h2 > a > span';
+  const newsList = await fetchEvaluateTextArray(page, newsListSelector);
+
+  await browser.close();
+
+  console.log('ページタイトル:' + title);
+  console.log(newsList);
+}
+
+
+const fetchEvaluateTextArray = async function(page, listSelector) {
+  return page.evaluate((selector) => {
+    const list = Array.from(document.querySelectorAll(selector));
+    return list.map((data) => data.innerText);
+  }, listSelector);
+}
+
+main();
