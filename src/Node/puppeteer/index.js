7eb65efe3bfa54981c66bceeed70bc0b102e8c7c
SugarShootingStar <ryu.8224@gmail.com>
2018-11-16 17:56:41 +0900

    fix:puppeteerのコードを他のサンプルと同じ動作をするように変更

--- src/Node/puppeteer/index.js
+++ src/Node/puppeteer/index.js
@@ -1,27 +1,36 @@
 const puppeteer = require('puppeteer');
 const URL = 'https://gigazine.net/';
 
-async function main(){
+async function main() {
+  const newsList = await fetchNewsList();
+  console.log(newsList);
+}
+
+async function fetchNewsList(){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(URL);
 
-  const title = await page.title();
-  const newsListSelector = '#section > div > section > div > h2 > a > span';
-  const newsList = await fetchEvaluateTextArray(page, newsListSelector);
+  const newsList = await extractionNews(page);
 
   browser.close();
-
-  console.log('ページタイトル:' + title);
-  console.log(newsList);
+  return newsList;
 }
 
 
-const fetchEvaluateTextArray = async function(page, listSelector) {
-  return page.evaluate((selector) => {
-    const list = Array.from(document.querySelectorAll(selector));
-    return list.map((data) => data.innerText);
-  }, listSelector);
+async function extractionNews(page) {
+  return page.evaluate(() => {
+    return Array.from(document.querySelectorAll('div.card > h2 > a'))
+            .map((value) => {
+              return {
+                title: value.title,
+                link: value.href
+              }
+            })
+            .filter((value) => {
+              return value.title !== ''
+            });
+  });
 }
 
 main();
