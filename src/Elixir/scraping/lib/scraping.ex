c1424d374dc435deac96441e92ac8d949435a32e
SugarShootingStar <ryu.8224@gmail.com>
2018-11-16 16:11:10 +0900

    feat:Elixir scraping fix

--- src/Elixir/scraping/lib/scraping.ex
+++ src/Elixir/scraping/lib/scraping.ex
@@ -6,5 +6,16 @@ defmodule Scraping do
     body = HTTPoison.get!(url).body
 
     newsList = Floki.find(body, "div.card > h2 > a")
+                |> Enum.map(&(extractionNews(&1)))
+    IO.inspect newsList
   end
+
+  # 必要な情報のみ抽出しMap化
+  def extractionNews(n) do
+    %{
+      "title" => Floki.attribute(n, "title"),
+      "link" => Floki.attribute(n, "href")
+    }
+  end
+
 end
